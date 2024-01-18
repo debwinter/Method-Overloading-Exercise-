e3a9603473a1efcea1eebdc1c598ed9234fd511e
Debra Winter <deb@debwinter.com>
2024-01-18 11:46:09 -0500

    submission for completed exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool isDollars)
+        {
+            if (a + b == 1 || a + b == -1)
+            {
+                return $"{a + b} dollar";
+            }
+            else
+            {
+                return $"{a + b} dollars";
+            }
         }
     }
 }
